734370c4f024663f0a9ff07d0b31e5e3bed21e18
keunpyo jeon <john.coner72@gmail.com>
2024-07-02 20:41:34 +0900

    first-commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let taskList = [];
+
+
+
+addButton.addEventListener("click",addTask);
+
+function addTask(){
+    let taskContent = taskInput.value;
+    taskList.push(taskContent);
+        console.log(taskList);
+        render();
+    
+}
+
+function render(){
+    let resultHTMl = "";
+    for(let i=0;i<taskList.length;i++){
+        resultHTMl += `<div class="task">
+                    <div>${taskList[i]}</div>
+                    <div>
+                        <button>Check</button>
+                        <button>Delete</button>
+                    </div>
+                </div>`;
+    }
+
+
+    document.getElementById("task-board").innerHTML = resultHTMl;
+}
